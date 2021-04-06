50dcc2ef03e8acfd04fa7c71ee3cfb399dc89f4f
leestephens12 <77406283+leestephens12@users.noreply.github.com>
2021-04-06 16:59:00 -0400

    Coach model class complete

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import models.Coach;
 import models.Player;
 
 public class Main {
     public static void main(String[] args) {
         Player lebronJames = new Player("Lebron","James", 206, 275, "Small Forward");
         System.out.println(lebronJames.toString());
+
+        Coach steveKerr = new Coach("Steve", "Kerr","head coach", 5);
+        System.out.println(steveKerr.toString());
     }
 }
 
--- src/models/Coach.java
+++ src/models/Coach.java
@@ -1,16 +1,32 @@
 package models;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Coach {
     private String firstName;
     private String lastName;
     private int yearsExperience;
+    private String coachingPosition;
+
+    public Coach(String firstName, String lastName, String coachingPosition, int yearsExperience) {
+        setFirstName(firstName.trim());
+        setLastName(lastName.trim());
+        setCoachingPosition(coachingPosition.trim());
+        setYearsExperience(yearsExperience);
+    }
 
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        firstName = firstName.trim();
+        if (firstName.isEmpty()) {
+            throw new IllegalArgumentException("You must input a name to continue");
+        } else {
+            this.firstName = firstName;
+        }
     }
 
     public String getLastName() {
@@ -18,7 +34,13 @@ public class Coach {
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        lastName = lastName.trim();
+        if(lastName.isEmpty()) {
+            throw new IllegalArgumentException("You must input a name to continue");
+        }
+        else {
+            this.lastName = lastName;
+        }
     }
 
     public int getYearsExperience() {
@@ -26,16 +48,32 @@ public class Coach {
     }
 
     public void setYearsExperience(int yearsExperience) {
-        this.yearsExperience = yearsExperience;
+        if(yearsExperience > 0 && yearsExperience < 50) {
+            this.yearsExperience = yearsExperience;
+        }
+        else {
+            throw new IllegalArgumentException("Coaches must have 0 to 50 years of experience");
+        }
     }
 
-    public int getCoachingPosition() {
+    public String getCoachingPosition() {
         return coachingPosition;
     }
 
-    public void setCoachingPosition(int coachingPosition) {
-        this.coachingPosition = coachingPosition;
+    public void setCoachingPosition(String coachingPosition) {
+        if(getValidCoachingPositions().contains(coachingPosition.toLowerCase())) {
+            this.coachingPosition = coachingPosition;
+        }
+        else {
+            throw new IllegalArgumentException("Your coaching position must be one of the following: " + getValidCoachingPositions() + ".");
+        }
     }
 
-    private int coachingPosition;
+    public static List<String> getValidCoachingPositions() {
+        return Arrays.asList("head coach", "assistant coach", "trainer");
+    }
+
+    public String toString() {
+        return firstName + " " + lastName + "\nCoaching Position: " + coachingPosition;
+    }
 }
